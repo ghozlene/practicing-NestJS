4664eae9dea848755f539106d7304d1027128e17
Achref <medachrefg@gmail.com>
2022-09-29 19:04:08 +0100

    creating the module and starting our Nest app

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,5 @@
 import { Controller, Get, Module } from "@nestjs/common";
+import { NestFactory } from "@nestjs/core";
 
 
 @Controller()
@@ -9,3 +10,17 @@ class AppController {
         return 'hi there';
     }
 }
+
+@Module({
+    controllers: [AppController]
+})
+class AppModule {
+
+}
+
+async function bootstrap() {
+    const app = await NestFactory.create(AppModule);
+
+    await app.listen(3000);
+}
+bootstrap();
