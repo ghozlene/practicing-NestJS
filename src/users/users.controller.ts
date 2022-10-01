6e6cc94772bb3d5a717ee86ca2abecf35cd56e8f
Achref <medachrefg@gmail.com>
2022-10-01 18:59:05 +0100

    finding and filtering records

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,5 +1,4 @@
-import { Body, Controller, Post } from '@nestjs/common';
-import { request } from 'express';
+import { Body, Controller, Get, Param, Post, Query } from '@nestjs/common';
 import { CreateUserDto } from './dtos/create-user.dto';
 import { UsersService } from './users.service';
 @Controller('auth')
@@ -12,9 +11,25 @@ export class UsersController {
 
         await this.usersService.create(body.email, body.password);
 
-
-
     }
+
+    @Get('/:id')
+    async findUser(
+        @Param('id') id: string) {
+        return await this.usersService.findOne(parseInt(id));
+    }
+
+    @Get()
+    async findAllUsers(
+
+        @Query('email') email: string
+    ) {
+
+        return this.usersService.find(email);
+    }
+
+
+
 
 
 }
