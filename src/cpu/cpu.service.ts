3cd6baba7266f5540dfb7f79889e4dd05333ec25
Achref <medachrefg@gmail.com>
2022-10-01 01:34:12 +0100

    using more Di between modules

--- src/cpu/cpu.service.ts
+++ src/cpu/cpu.service.ts
@@ -4,4 +4,10 @@ import { PowerService } from 'src/power/power.service';
 export class CpuService {
 
     constructor(private powerService: PowerService) { }
+
+    compute(a: number, b: number) {
+        console.log('adding 10 watts of power from powerSerivce');
+        this.powerService.supplyPower(10);
+        return a + b;
+    }
 }
--- src/disk/disk.service.ts
+++ src/disk/disk.service.ts
@@ -1,4 +1,15 @@
 import { Injectable } from '@nestjs/common';
-
+import { PowerService } from 'src/power/power.service';
 @Injectable()
-export class DiskService {}
+export class DiskService {
+
+    constructor(private porwerService: PowerService) {
+
+    }
+
+    getData() {
+        console.log('Drawing 20 watts of power from powerService');
+        this.porwerService.supplyPower(20);
+        return 'data';
+    }
+}
