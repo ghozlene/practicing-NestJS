c109049d8b822b0604624b2bfe346474e89d3df5
Achref <medachrefg@gmail.com>
2022-10-01 16:40:49 +0100

    setting up body validation

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,14 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { CreateUserDto } from './dtos/create-user.dto';
+@Controller('auth')
+export class UsersController {
 
-@Controller('users')
-export class UsersController {}
+
+    @Post('/signup')
+    createUser(@Body() body: CreateUserDto) {
+
+        console.log(body);
+    }
+
+
+}
