c6c21f00abbfdb8492142a3809c5622c03f370de
Achref <medachrefg@gmail.com>
2022-10-01 15:52:24 +0100

    viewing DB's contents with extension sqlite

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,14 +3,15 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { ReportsModule } from './reports/reports.module';
-
 import { TypeOrmModule } from '@nestjs/typeorm';
 
+import { User } from './users/user.entity';
+import { Report } from './reports/report.entity';
 @Module({
   imports: [UsersModule, ReportsModule, TypeOrmModule.forRoot({
     type: 'sqlite',
     database: 'db.sqlite',
-    entities: [],
+    entities: [User, Report],
     synchronize: true
   })],
   controllers: [AppController],
