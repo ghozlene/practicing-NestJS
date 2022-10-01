857623b4f14e30126eaa7ea46c7a5a63a13ee59c
Achref <medachrefg@gmail.com>
2022-10-01 17:30:34 +0100

    creating and saving data

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,14 +1,19 @@
 import { Body, Controller, Post } from '@nestjs/common';
+import { request } from 'express';
 import { CreateUserDto } from './dtos/create-user.dto';
+import { UsersService } from './users.service';
 @Controller('auth')
 export class UsersController {
-
+    constructor(private usersService: UsersService) { }
 
     @Post('/signup')
-    createUser(
+    async createUser(
         @Body() body: CreateUserDto) {
 
-        console.log(body);
+        await this.usersService.create(body.email, body.password);
+
+
+
     }
 
 
